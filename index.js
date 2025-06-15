c0626fa3650bfef7d82c490a7a34e845926cc240
Trịnh Phú Thái <140522495+thaiphu05@users.noreply.github.com>
2025-06-15 18:12:32 +0700

    Initial commit

--- index.js
+++ index.js
@@ -23,7 +23,24 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  let dateObj;
+  if (!date) {
+    dateObj = new Date();
+  } else if (/^\d+$/.test(date)) {
+    dateObj = new Date(parseInt(date));
+  } else {
+    dateObj = new Date(date);
+  }
+  if (dateObj.toString() === 'Invalid Date') {
+    return res.json({error: 'Invalid Date'});
+  }
+  else {
+  res.json({unix : dateObj.getTime(), 
+            utc : dateObj.toUTCString()});
+  }
+});
 
 
 // Listen on port set in environment variable or default to 3000
